5a8495a8e88ce4424a175637efeccc52a750577a
Cléber Severo <clebersevero1998@gmail.com>
2025-04-10 08:15:13 -0300

    feat: routes config

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import { buildRoutePath } from './utils/build-route-path.js';
+
+export const routes = [
+	{
+		method: 'GET',
+		path: buildRoutePath('/tasks'),
+		handler: (req, res) => {
+			return res.end('get todos');
+		},
+	},
+];
--- src/server.js
+++ src/server.js
@@ -1,10 +1,26 @@
 import http from 'http';
-import { json } from './middlewares/json';
+import { json } from './middlewares/json.js';
+import { routes } from './routes.js';
 
 const server = http.createServer(async (req, res) => {
+	const { method, url } = req;
+
 	await json(req, res);
 
-	return res.writeHead(200).end('Hello World');
+	const route = routes.find((route) => route.method === method && route.path.test(url));
+
+	if (route) {
+		const routeParam = req.url.match(route.path);
+
+		const { query, ...params } = routeParam.groups;
+
+		req.params = params;
+		req.query = query ? extractQueryParams(query) : {};
+
+		return route.handler(req, res);
+	}
+
+	return res.writeHead(404).end('Not found');
 });
 
 server.listen(3333);
