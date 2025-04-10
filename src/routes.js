cdb051a06cd00a887d6882335823b424c62f64a3
Cléber Severo <clebersevero1998@gmail.com>
2025-04-10 08:25:25 -0300

    feat: add database

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,18 @@
+import { DataBase } from './database.js';
 import { buildRoutePath } from './utils/build-route-path.js';
+
+const dataBase = new DataBase();
 
 export const routes = [
 	{
 		method: 'GET',
 		path: buildRoutePath('/tasks'),
 		handler: (req, res) => {
-			return res.end('get todos');
+			const { search } = req.query;
+
+			const tasks = dataBase.select('tasks', search ? { name: search, email: search } : null);
+
+			return res.end(JSON.stringify(tasks));
 		},
 	},
 ];
